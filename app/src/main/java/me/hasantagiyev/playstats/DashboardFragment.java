6e3d1abc51a0d41c08274265a0f511beb756ed1d
mahwd <lazure.net@gmail.com>
2019-01-06 04:44:12 +0400

    add payment page

--- app/src/main/java/me/hasantagiyev/playstats/DashboardFragment.java
+++ app/src/main/java/me/hasantagiyev/playstats/DashboardFragment.java
@@ -1,20 +1,41 @@
 package me.hasantagiyev.playstats;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class DashboardFragment extends Fragment {
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        FloatingActionButton add_payment = view.findViewById(R.id.add_payment);
+        add_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddPayment.class);
+                startActivity(intent);
+                Toast.makeText( getContext(), "Changed", Toast.LENGTH_SHORT).show();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return view;
     }
+
+
 }
